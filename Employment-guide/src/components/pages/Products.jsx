a065b6efa33d7809df5e01d74fecc80abfd48b31
Nyelus101 <cmuonyelu@gmail.com>
2024-02-27 20:54:05 +0100

    update

--- Employment-guide/src/components/pages/Products.jsx
+++ Employment-guide/src/components/pages/Products.jsx
@@ -4,3 +4,16 @@ import '../../App.css';
 export default function Products() {
   return <h1 className='products'>PRODUCTS</h1>;
 }
+
+/*import React from 'react';
+import '../../App.css';
+import Scoreboard from '../../Quiz-app/Components/Scoreboard';
+
+export default function Products() {
+  return (
+    <>
+      <Scoreboard />
+      {/*<h1 className='products'>PRODUCTS</h1>*//*}
+    </>
+  );
+}*/
--- Employment-guide/src/components/pages/Services.jsx
+++ Employment-guide/src/components/pages/Services.jsx
@@ -1,6 +1,47 @@
-import React from 'react';
+
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Exam from '../../Quiz-app/Exam.jsx';
+//import QuizApp from '../../../Test/QuizApp.jsx';
+//import "../../../Test/index.scss";
+
+function Services() {
+  const values = [true];
+  const [fullscreen, setFullscreen] = useState(true);
+  const [show, setShow] = useState(false);
+
+  function handleShow(breakpoint) {
+    setFullscreen(breakpoint);
+    setShow(true);
+  }
+
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      {values.map((v, idx) => (
+        <Button key={idx} className="me-2 mb-2" size="lg" onClick={() => handleShow(v)}>
+          Click to Start Quiz
+          {typeof v === 'string' && `below ${v.split('-')[0]}`}
+        </Button>
+      ))}
+      <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>THE SECP QUIZ</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Exam />
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+}
+
+export default Services;
+
+
+/*import React from 'react';
 import '../../App.css';
 
 export default function Services() {
   return <h1 className='services'>SERVICES</h1>;
-}
+}*/
